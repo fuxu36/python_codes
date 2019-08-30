d3032b38b2c5aaa511166541c432f7a4e6419a69
fuxu3636 <snowxue9092@gmail.com>
2019-08-30 15:11:27 +1200

    Isolation forest for outlier detection.

--- isolation_forest.py
+++ isolation_forest.py
@@ -11,9 +11,8 @@ import time
 
 
 start = time.time()
-#file = '/Users/xue/Desktop/Farrago/Datasets/Netlogx Training Data/part-00190-147ed921-7fa3-436f-928b-51de852735be-c000.csv'
-#file = '/Users/xue/Desktop/Farrago/Datasets/Netlogx Training Data/combined_csv.csv'
-file = '/Users/xue/Desktop/Farrago/Datasets/house price demo/kc_house_data.csv'
+
+file = '/Users/xue/Desktop/Farrago/Datasets/Netlogx Training Data/combined_csv.csv'
 
 spark = SparkSession \
         .builder.master("local[*]") \
@@ -21,20 +20,18 @@ spark = SparkSession \
         .getOrCreate()
 
 data = spark.read.csv(file
-                      , header=True
+                      , header=False
                       , inferSchema=True)
 print('Number of Rows: {}'.format(data.count()))
 
 columns_not_str = [item[0] for item in data.dtypes if not item[1].startswith('string')]
 print(columns_not_str)
 
-# convert value in columns to an array
-# then convert array to vector
+# convert columns to vectors
 df = data.select(array(columns_not_str).alias('features'))
 to_vector = udf(lambda a: Vectors.dense(a), VectorUDT())
 df = df.select(to_vector("features").alias("features"))
 df.show()
-
 
 # convert value in columns to vector directly
 # vecAssembler = VectorAssembler(inputCols=columns_not_str, outputCol="features")
@@ -43,43 +40,24 @@ df.show()
 
 # Init an IForest Object and Fit on a given data frame
 iforest = IForest(maxDepth=10
-                  , numTrees=10
+                  , numTrees=100
                   , bootstrap=False
-                  #, approxQuantileRelativeError=0.05
-                  # , contamination=0.05
-                  , maxSamples=256
-                  , maxFeatures = 20)
+                  , approxQuantileRelativeError=0.01
+                  #, contamination=0.05
+                  , maxSamples=1000
+                  , maxFeatures=20)
 
 model = iforest.fit(df)
 
-# Check if the model has summary or not, the newly trained model has the summary info
-model.hasSummary
-# Show model summary
-summary = model.summary
-# Show the number of anomalies
-print(summary.numAnomalies)
-# Predict for a new data frame based on the fitted model
+if model.hasSummary:
+
+    summary = model.summary
+    print('Number of anomalier: {}'.format(summary.numAnomalies))
+else:
+    print('Does not have summary')
+
 transformed = model.transform(df)
-
-# Collect spark data frame into local df
-rows = transformed.select(transformed.columns)
-temp_path = tempfile.mkdtemp()
-iforest_path = temp_path + "/iforest"
-
-# Save the iforest estimator into the path
-iforest.save(iforest_path)
-
-# Load iforest estimator from a path
-loaded_iforest = IForest.load(iforest_path)
-model_path = temp_path + "/iforest_model"
-
-# Save the fitted model into the model path
-model.save(model_path)
-
-# Load a fitted model from a model path and  predict a new data frame
-loaded_model = IForestModel.load(model_path)
-loaded_model.hasSummary
-loaded_model.transform(df).show()
+transformed.show()
 
 end = time.time()
 print(end-start)
